175e2f46e7f8726c7fbf6c770cbe65efce4f72be
es-cheryomushkin <es-cheryomushkin@users.noreply.github.com>
2025-08-31 06:45:19 +0000

    Deploying to gh-pages from @ es-cheryomushkin/OOP@8cc799b0fa1171efde2fe97386b5ff4c606d4a22 🚀

---
+++ Task_1_1_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.cheremushkin","l":"Sample"},{"p":"ru.nsu.cheremushkin","l":"Test"}];updateSearchResults();
